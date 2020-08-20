37b50b3a9fb964f52f3aa336048126c9ec5ecbcb
gaurav <gauravsaini964@gmail.com>
2020-08-20 19:42:28 +0530

    Add registration API
    Signed-off-by: gaurav <gauravsaini964@gmail.com>

--- api/models.py
+++ api/models.py
@@ -6,13 +6,13 @@ from django.db import models
 class AuthUser(models.Model):
     password = models.CharField(max_length=128)
     last_login = models.DateTimeField(blank=True, null=True)
-    is_superuser = models.BooleanField()
+    is_superuser = models.BooleanField(default=False)
     username = models.CharField(unique=True, max_length=150)
     first_name = models.CharField(max_length=30)
-    email = models.CharField(max_length=254)
-    is_staff = models.BooleanField()
-    is_active = models.BooleanField()
-    date_joined = models.DateTimeField()
+    email = models.CharField(max_length=254, unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
     last_name = models.CharField(max_length=150)
 
     class Meta:
--- api/views/auth.py
+++ api/views/auth.py
@@ -4,8 +4,8 @@ from rest_framework import status
 from django.contrib.auth.hashers import make_password
 
 # Misc Imports
-import bcrypt
 from api.utils.jwt_generate import jwt_payload_handler
+import datetime
 
 # Models Imports
 from api.models import AuthUser
@@ -15,16 +15,33 @@ class RegisterView(APIView):
     @staticmethod
     def post(request):
         try:
-            name = request.data.get("name", None)
+            first_name = request.data.get("first_name", None)
+            last_name = request.data.get("last_name", None)
             email = request.data.get("email", None)
             password = request.data.get("password", None)
             hashed_pass = make_password(password)
-            print(hashed_pass)
-            response = {
-                "message": "Something went wrong",
-                "status": status.HTTP_201_CREATED,
-                "result": {},
-            }
+            user_name = email.split("@")[0]
+            user_obj = AuthUser.objects.create(
+                password=hashed_pass,
+                last_login=datetime.datetime.now(),
+                email=email,
+                last_name=last_name,
+                username=user_name,
+                first_name=first_name,
+            )
+            if user_obj:
+                user_info = {"user_id": user_obj.id}
+                response = {
+                    "message": "User signed up successfully",
+                    "status": status.HTTP_201_CREATED,
+                    "result": user_info,
+                }
+            else:
+                response = {
+                    "message": "Could not create entry",
+                    "status": status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    "result": {},
+                }
             return Response(response, response["status"])
 
         except:
